100f14b36cff0a41b255a1faee96e19f3490058f
Nikita Kashko <kashkonikev@gmail.com>
2025-04-26 22:51:54 +0300

    Vse rabotaet

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ from collections import defaultdict
 from api.database import AsyncSessionLocal
 from api.models import Reading
 
-from sqlalchemy import insert
+from sqlalchemy import insert, text
 
 from math import ceil
 
@@ -23,8 +23,13 @@ async def insert_in_chunks(session, data):
     
     for i in range(chunks):
         chunk = data[i*CHUNK_SIZE : (i+1)*CHUNK_SIZE]
-        await session.execute(insert(Reading), chunk)
-        await session.commit()
+        try:
+            await session.execute(insert(Reading), chunk)
+            await session.commit()  # Явный коммит после каждой порции
+        except Exception as chunk_error:
+            await session.rollback()
+            print(f"Chunk {i} error: {chunk_error}")
+            raise
 
 def parse_excel_to_list(file_content: bytes) -> List[List]:
     try:
@@ -35,27 +40,27 @@ def parse_excel_to_list(file_content: bytes) -> List[List]:
 
         dates = [column_headers[i] for i in range(3, len(column_headers), 4)]
 
-        # print(dates)
+        print(len(dates))
 
         readings_list = list()
         
         data_list = df.values.tolist()
 
-        for i in range(2, len(data_list)):
+        for i in range(1, len(data_list)):
             device_id = str(int(data_list[i][1]))
             for j in range(len(dates)):
                 readings_list.append(
                     {
                         'time': dates[j],
                         'device_id': device_id,
-                        'a_plus': data_list[i][j + 3],
-                        'a_minus': data_list[i][j + 4],
-                        'r_plus': data_list[i][j + 5],
-                        'r_minus': data_list[i][j + 6],
+                        'a_plus': data_list[i][j * 4 + 3],
+                        'a_minus': data_list[i][j * 4 + 4],
+                        'r_plus': data_list[i][j * 4 + 5],
+                        'r_minus': data_list[i][j * 4 + 6],
                     }
                 )
         readings_devices_id = [elem['device_id'] for elem in readings_list]
-        print(readings_devices_id)
+        print(len(readings_list))
         return readings_list
         
     except Exception as e:
@@ -80,3 +85,13 @@ async def upload_excel(file: UploadFile = File(...)):
         except Exception as e:
             await session.rollback()
             raise HTTPException(500, detail=f"Database error: {str(e)}")
+
+
+@app.get("/test-db")
+async def test_db():
+    async with AsyncSessionLocal() as session:
+        try:
+            result = await session.execute(text("SELECT 1"))
+            return {"status": "OK", "result": result.scalar()}
+        except Exception as e:
+            raise HTTPException(500, detail=str(e))
